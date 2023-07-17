093110b5a3dc8beae46a53f9f2134a26b4b014c7
Sayyad_Akhundzada <sayyadakhundzada@gmail.com>
2023-07-17 12:38:58 -0700

    Pages are updated

--- src/test/java/com/alzir/step_definitions/VytrackLogin_StepDefinitions.java
+++ src/test/java/com/alzir/step_definitions/VytrackLogin_StepDefinitions.java
@@ -1,2 +1,25 @@
-package com.alzir.step_definitions;public class VytrackLogin_StepDefinitions {
+package com.alzir.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class VytrackLogin_StepDefinitions {
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user enters the driver information")
+    public void user_enters_the_driver_information() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user should be able to login")
+    public void user_should_be_able_to_login() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }
