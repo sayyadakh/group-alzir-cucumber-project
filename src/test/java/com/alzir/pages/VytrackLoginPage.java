b0315fe4d59f21fb8dbc961bc58b6821a3f29790
seydaOzturk <aslanseyda91@gmail.com>
2023-07-18 00:25:58 -0400

    created login method

--- src/test/java/com/alzir/pages/VytrackLoginPage.java
+++ src/test/java/com/alzir/pages/VytrackLoginPage.java
@@ -11,6 +11,22 @@ public class VytrackLoginPage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
+    @FindBy(id="prependedInput")
+    public WebElement usernameInput;
+
+
+    @FindBy(id="prependedInput2")
+    public WebElement passwordInput;
+
+    @FindBy(name = "_submit")
+    public WebElement loginButton;
+
+    public void login(String userNameStr, String passwordStr) {
+        usernameInput.sendKeys(userNameStr);
+        passwordInput.sendKeys(passwordStr);
+        loginButton.click();
+        // verification that we logged
+    }
 
 
 }
--- src/test/java/com/alzir/step_definitions/VytrackLogin_StepDefinitions.java
+++ src/test/java/com/alzir/step_definitions/VytrackLogin_StepDefinitions.java
@@ -1,5 +1,8 @@
 package com.alzir.step_definitions;
 
+import com.alzir.pages.VytrackLoginPage;
+import com.alzir.utilities.ConfigurationReader;
+import com.alzir.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -8,18 +11,33 @@ public class VytrackLogin_StepDefinitions {
 
     @Given("user is on the login page")
     public void user_is_on_the_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @When("user enters the driver information")
-    public void user_enters_the_driver_information() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("user should be able to login")
-    public void user_should_be_able_to_login() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Driver.getDriver().get(ConfigurationReader.getProperty("vyTrackUrl"));
     }
 
+
+    @When("user logged in as {string}")
+    public void userLoggedInAs(String userType ) {
+        String username = null;
+        String password = null;
+
+        if(userType.equalsIgnoreCase("driver")){
+            username = ConfigurationReader.getProperty("driver_username");
+            password = ConfigurationReader.getProperty("driver_password");
+        }else if(userType.equalsIgnoreCase("sales Manager")){
+            username = ConfigurationReader.getProperty("sales_manager_username");
+            password = ConfigurationReader.getProperty("sales_manager_password");
+        }else if(userType.equalsIgnoreCase("store Manager")){
+            username = ConfigurationReader.getProperty("store_manager_username");
+            password = ConfigurationReader.getProperty("store_manager_password");
+        }
+
+        VytrackLoginPage loginPage = new VytrackLoginPage();
+        loginPage.login(username,password);
+    }
+
+       @When("user logged in with username as {string} and password {string}")
+       public void userLoggedInWithUsernameAsAndPassword(String username, String password) {
+       VytrackLoginPage loginPage = new VytrackLoginPage();
+        loginPage.login(username,password);
+    }
 }
