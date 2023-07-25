d8b98e113c75e0ac90d0540f5872575262404ec1
NasibaU <naasibaa@yahoo.com>
2023-07-25 12:59:36 -0400

    updating codes

--- src/test/java/com/alzir/step_definitions/DataTables_StepDefinitions.java
+++ src/test/java/com/alzir/step_definitions/DataTables_StepDefinitions.java
@@ -15,17 +15,21 @@ import java.util.List;
 public class DataTables_StepDefinitions {
     VytrackDashboardPage vytrackDashboardPage=new VytrackDashboardPage();
 
+
     @Then("user should be able to see following modules")
     public void user_should_be_able_to_see_following_modules(List<String> expectedModules ) {
 
-        BrowserUtils.sleep(5);
+
 
         List<String> actualModules = new ArrayList<>();
 
         for (WebElement eachModule : vytrackDashboardPage.allTopModules) {
 
+            BrowserUtils.sleep(5);
+
             actualModules.add(eachModule.getText());
         }
+        
         Assert.assertEquals(expectedModules,actualModules);
 
     }
