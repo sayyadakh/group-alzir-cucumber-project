83dc5033ce953087ff4f52cbf37622a837e7a790
Korhanguvn <korhanguvn@gmail.com>
2023-07-25 19:18:00 -0400

    all done

--- src/test/java/com/alzir/pages/VytrackDashboardPage.java
+++ src/test/java/com/alzir/pages/VytrackDashboardPage.java
@@ -20,6 +20,11 @@ public class VytrackDashboardPage {
     @FindBy(xpath = "//span[@class='title title-level-1']")
     public List<WebElement> allTopModules;
 
+    @FindBy(linkText = "Vehicles")
+    public WebElement vehicleBtn;
+
+    @FindBy(xpath = "//span[.='Vehicle Contracts']")
+    public WebElement vehicleContracts;
 
 
 }
