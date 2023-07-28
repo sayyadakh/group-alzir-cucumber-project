835db0a59ae7f81163d0be0a3d9906116f191bc2
Sayyad_Akhundzada <sayyadakhundzada@gmail.com>
2023-07-28 11:12:18 -0700

    new BasePage created

---
+++ src/test/java/com/alzir/pages/BasePage.java
@@ -0,0 +1,48 @@
+package com.alzir.pages;
+
+import com.alzir.utilities.BrowserUtils;
+import com.alzir.utilities.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//h1[@class='oro-subtitle']")
+    public WebElement submodulePageHeader;
+
+
+    public void navigateToModule(String tab, String module){
+        BrowserUtils.sleep(5);
+
+        String tabLocatorXPath = "//span[contains(text(),'" + tab + "') and @class='title title-level-1']";
+        String moduleLocatorXPath = "//span[contains(text(),'" + module + "') and @class='title title-level-2']";
+        BrowserUtils.hover(Driver.getDriver().findElement(By.xpath(tabLocatorXPath)));
+        //Driver.getDriver().findElement(By.xpath(tabLocatorXPath));
+        BrowserUtils.sleep(5);
+        Driver.getDriver().findElement(By.xpath(moduleLocatorXPath)).click();
+        BrowserUtils.sleep(2);
+    }
+
+    public void verifySubmoduleHeaderText(String submoduleHeaderText){
+       BrowserUtils.sleep(5);
+        System.out.println("submoduleHeaderText = " + submoduleHeaderText);
+        System.out.println("submoduleHeaderText from UI = " + submodulePageHeader.getText());
+//        Assert.assertTrue(submoduleHeaderText.contains(submodulePageHeader.getText()));
+        String[] submoduleHeaderTextArr = submodulePageHeader.getText().split(submoduleHeaderText);
+        System.out.println("submoduleHeaderTextArr length = " + submoduleHeaderTextArr.length);
+        System.out.println("submoduleHeaderTextArr = " + submoduleHeaderTextArr[0]);
+        for (String each : submoduleHeaderTextArr) {
+            if (each.equals(submodulePageHeader.getText())){
+                Assert.assertEquals(submoduleHeaderText, submodulePageHeader.getText());
+            }
+        }
+    }
+
+}
---
+++ src/test/resources/features/BasePagePractice.feature
@@ -0,0 +1,7 @@
+Feature: user should be able to navigate to any module or any page dynamically
+@Mikail
+  Scenario: verify users are able to navigate to sub-modules dynamically
+    Given user is on the login page
+    When user logged in as "sales manager"
+    And user hovers over to a "Fleet" and navigates to a "Vehicle Costs"
+    And user verifies "x"
